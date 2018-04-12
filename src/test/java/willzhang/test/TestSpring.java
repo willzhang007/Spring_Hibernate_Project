5ff071a8d41a7274713c4b033908dab285d80fa0
Lichao Zhang <lichaozhang007@gmail.com>
2018-04-12 00:54:39 -0400

    fifth commit

--- src/test/java/willzhang/test/TestSpring.java
+++ src/test/java/willzhang/test/TestSpring.java
@@ -1,4 +1,4 @@
-package rml.test;
+package willzhang.test;
 
 import java.util.List;
 
@@ -11,9 +11,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.alibaba.fastjson.JSON;
 
-import rml.model.vo.DataGrid;
-import rml.model.vo.User;
-import rml.service.UserServiceI;
+import willzhang.model.vo.DataGrid;
+import willzhang.model.vo.User;
+import willzhang.service.UserServiceI;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:spring.xml","classpath:spring-hibernate.xml" })
